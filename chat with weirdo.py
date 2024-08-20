89c5b45be4055ebc658821dfd91e49d55f95d931
sortakalpuyu <116273898+sortakalpuyu@users.noreply.github.com>
2024-08-20 20:23:20 +0300

    Add files via upload

---
+++ chat with weirdo.py
@@ -0,0 +1,62 @@
+import time
+
+print("        WELCOME TO MY \"CHAT WITH WEIRDO\" SIMULATION")
+time.sleep(2)
+print("                      Let's Start!")
+time.sleep(1)
+
+annoying_word = input("Write an annoying word: ")
+wname = input("Write a weirdo name: ") + ": "
+name = input("Write a normal name: ") + ": "
+print()
+print(name + "hello")
+time.sleep(1)
+
+print(wname + annoying_word)
+time.sleep(1.5)
+
+print(name + "wassuo")
+time.sleep(0.6)
+
+print(name + "p")
+time.sleep(1.3)
+
+print(wname + annoying_word)
+time.sleep(1)
+
+print(name + "stop saying " + annoying_word)
+time.sleep(1.4)
+
+print(wname + annoying_word + "?")
+time.sleep(0.6)
+
+print(name + "YESS")
+time.sleep(2.4)
+
+print(wname + annoying_word)
+time.sleep(0.4)
+
+print(name)
+print("           f   f          ")
+print("      f            f      ")
+print("    f   __      __   f    ")
+print("   f     _\    /_     f   ")
+print("   f        _         f   ")
+print("    f      / \       f    ")
+print("      f    \_/     f      ")
+print("         f     f          ")
+print("                          ")
+
+time.sleep(5)
+print("Simulation will close in: 5")
+time.sleep(1)
+print("                          " + "4")
+time.sleep(1)
+print("                          " + "3")
+time.sleep(1)
+print("                          " + "2")
+time.sleep(1)
+print("                          " + "1")
+time.sleep(1)
+print("                          " + "0")
+time.sleep(0.2)
